fc0ea349a78997793b3579f07be7108e5c38568c
Kevin Murphy <kmurphy5@mail.umw.edu>
2015-01-30 16:52:21 -0500

    changed subreddit to comments

--- snoopify.py
+++ snoopify.py
@@ -29,7 +29,7 @@ else:
         replies = filter(None, replies)
 
 
-all_comments = r.get_subreddit('tessst')
+all_comments = r.get_comments('tessst')
 
 
 #Maintain a list of comments that have been translated so we don't spam
